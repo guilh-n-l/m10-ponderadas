218505a85c31bb91a2a291716b88412ea9607fb8
guilh-n-l <guilherme.lima@sou.inteli.edu.br>
2024-11-05 23:12:21 -0300

    (P5) Update

--- p5/main.c
+++ p5/main.c
@@ -1,62 +1,86 @@
+#include <string.h>
 #include <time.h>
 #include <stdlib.h>
 #include <omp.h>
 #include <stdio.h>
 
-void get_thread_range(int thread_num, int num_threads, int arr_len, int* range) {
-    int slice_size = arr_len / num_threads;
-    range[0] = thread_num * slice_size;
-    if (thread_num == num_threads - 1) {
-        range[1] = arr_len - 1;
-    } else {
-        range[1] = range[0] + (slice_size - 1);
-    }
-    if (range[1] < range[0]) {
-        range[1] = range[0];
-    }
+/**
+ * @brief Set thread start index and how many numbers to sum
+ *
+ * This function computes the start and amount for thread to calculate sum in array
+ *
+ * @param threadNum Index of thread
+ * @param numThreads Total number of executing threads
+ * @param arrLen Length of array
+ * @param range Pointer to start of range array
+ */
+void getThreadRange(int threadNum, int numThreads, int arrLen, int* range) {
+    int sliceLen = arrLen / numThreads;
+    range[0] = threadNum * sliceLen;
+    range[1] = threadNum == numThreads - 1 ? arrLen - range[0] : (arrLen < range[0] ? 1 : sliceLen);
 }
 
-int sum_thread_results(int num_threads, int arr_len, int* arr) {
-    int collector = 0;
-    int slice_size = arr_len / num_threads;
-    for (int thread_num = 0; thread_num < num_threads; thread_num++) {
-        collector += arr[thread_num * slice_size];
+int sumArray(int arrLen, int* arr) {
+    int counter = 0;
+    for (int i = 0; i < arrLen; i++) {
+        counter += arr[i];
     }
-    return collector;
+    return counter;
 }
 
 int main(int argc, char* argv[]) {
     srand(time(NULL));
-    int arr_len = atoi(argv[1]);
-    if (arr_len <= 0) {
+
+    int arrLen = atoi(argv[1]);
+    if (arrLen <= 0) {
         printf("0\n");
         return 0;
     }
+
     printf("Array: [ ");
-    int *arr = malloc(arr_len * sizeof(int));
-    for (int i=0; i < arr_len; i++) {
+    int *arr = malloc(arrLen * sizeof(int));
+
+    for (int i=0; i < arrLen; i++) {
         arr[i] = rand() % 100;
         printf("%d ", arr[i]);
     }
+
     printf("]\n");
-    int num_threads = 1;
+
+    int numThreads = -1;
+
+    int  *threadsArr;
     #pragma omp parallel
     {
-        int thread_num = omp_get_thread_num();
-        if (thread_num + 1 <= arr_len) {
-            if (omp_get_num_threads() > arr_len) {
-                num_threads = arr_len;
-            } else {
-                num_threads = omp_get_num_threads();
+        if (numThreads == -1) {
+            #pragma omp critical
+            {
+                int threads = omp_get_num_threads();
+                numThreads =  threads > arrLen ? arrLen : threads;
+                threadsArr = malloc(numThreads * sizeof(int));
             }
-            int arr_thread[2];
-            get_thread_range(thread_num, num_threads, arr_len, arr_thread);
-            for (int i = arr_thread[0] + 1; i < arr_thread[1]; i++) {
-                arr[arr_thread[0]] += i;
-            }
+        }
+    }
+    double s = omp_get_wtime();
+    #pragma omp parallel
+    {
+
+        int threadNum = omp_get_thread_num();
+
+        if (threadNum + 1 <= arrLen) {
+            int threadRangeArr[2];
+
+            getThreadRange(threadNum, numThreads, arrLen, threadRangeArr);
+
+            threadsArr[threadNum] = sumArray(threadRangeArr[1], &arr[threadRangeArr[0]]);
         }   
     }
-    printf("\nSum: %d\nN threads used: %d\n", sum_thread_results(num_threads, arr_len, arr), num_threads);
+
+    printf("\nN threads used: %d\nSum: %d\nElapsed time: %.8f\n\n", numThreads, sumArray(numThreads, threadsArr), omp_get_wtime() - s);
+    free(threadsArr);
+
+    s = omp_get_wtime();
+    printf("N threads used: 1\nSum: %d\nElapsed time: %.8f\n", sumArray(arrLen, arr), omp_get_wtime() - s);
     free(arr);
     return 0;
 }

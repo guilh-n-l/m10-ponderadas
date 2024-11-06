d6e8d4d716d16a105ac72fbb3e4aa95118b17578
guilh-n-l <guilherme.lima@sou.inteli.edu.br>
2024-11-06 15:25:20 -0300

    (P5) Finalizando codigo

--- p5/main.c
+++ p5/main.c
@@ -1,9 +1,9 @@
-#include <string.h>
 #include <time.h>
 #include <stdlib.h>
 #include <omp.h>
 #include <stdio.h>
 #define MAX(a,b) (((a)>(b))?(a):(b))
+
 
 /**
  * @brief Set thread start index and how many numbers to sum
@@ -21,7 +21,23 @@ void getThreadRange(int threadNum, int numThreads, int arrLen, int* range) {
     range[1] = threadNum == numThreads - 1 ? arrLen - range[0] : (arrLen < range[0] ? 1 : sliceLen);
 }
 
-int sumArray(int arrLen, int* arr) {
+
+/**
+ * @brief Sum all numbers inside an array
+ *
+ * This function computes the sum of every integer inside an array
+ *
+ * @param arrLen Length of array
+ * @param arr Pointer to start of array
+ *
+ * @return Sum of integers in array
+ */
+int sumArray(unsigned int arrLen, int* arr) {
+    if (arrLen == 0) {
+        free(arr);
+        abort();
+    }
+
     int counter = 0;
     for (int i = 0; i < arrLen; i++) {
         counter += arr[i];
@@ -29,24 +45,91 @@ int sumArray(int arrLen, int* arr) {
     return counter;
 }
 
-int main(int argc, char* argv[]) {
+
+/**
+ * @brief Get random array from time(NULL)
+ *
+ * This function computes a N sized array with random integers from min to max (exclusive)
+ *
+ * @param n Size of array
+ * @param arr Array to put integers into
+ * @param min Minimum integer (inclusive)
+ * @param max Max integer (exclusive)
+ */
+void randomArray(unsigned int n, int *arr, int min, int max) {
     srand(time(NULL));
 
-    int arrLen = atoi(argv[1]);
-    if (arrLen <= 0) {
-        printf("0\n");
-        return 0;
+    if (n == 0 || min > max) {
+        free(arr);
+        abort();
     }
 
-    printf("Array: [ ");
-    int *arr = malloc(arrLen * sizeof(int));
+    #pragma omp target map(to: arr[0:n])
+    {
+        #pragma omp parallel for 
+        for (int i=0; i < n; i++) {
+            arr[i] = rand() % (max - min) + min;
+        }
+    }
+}
 
-    for (int i=0; i < arrLen; i++) {
-        arr[i] = rand() % 100;
+
+/**
+ * @brief Adds all numbers inside an array with multiple threads
+ *
+ * This function computes the sum of every integer inside an array using multiple threads
+ *
+ * @param arrLen Length of array
+ * @param arr Pointer to start of array
+ *
+ * @return Sum of integers in array
+ */
+int sumArrayMultithread(unsigned int arrLen, int *arr, unsigned int numThreads) {
+
+    if (numThreads == 0 || arrLen == 0) {
+        free(arr);
+        abort();
+    }
+
+    int collector = 0;
+
+    #pragma omp parallel num_threads(numThreads) reduction(+:collector)
+    {
+        int threadNum = omp_get_thread_num();
+
+        int threadRangeArr[2];
+        getThreadRange(threadNum, numThreads, arrLen, threadRangeArr);
+        #pragma omp atomic
+        collector += sumArray(threadRangeArr[1], &arr[threadRangeArr[0]]);
+    }
+
+    return collector;
+}
+
+
+/**
+ * @brief Print an array of integers
+ *
+ * This function prints an array of integers to stdin
+ *
+ * @param arrLen Length of array
+ * @param arr Pointer to start of array
+ */
+void printArrayString(unsigned int arrLen, int* arr) {
+    printf("[ ");
+    for (int i = 0; i < arrLen; i++) {
         printf("%d ", arr[i]);
     }
-
     printf("]\n");
+}
+
+
+int main(int argc, char* argv[]) {
+    unsigned int arrLen = (unsigned int)atoi(argv[1]);
+
+    int *arr = malloc(arrLen * sizeof(int));
+    randomArray(arrLen, arr, -100, 100);
+    printArrayString(arrLen, arr);
 
     int numThreads = -1;
 
@@ -60,16 +143,7 @@ int main(int argc, char* argv[]) {
     }
 
     double s = omp_get_wtime();
-    int collector = 0;
-    #pragma omp parallel num_threads(numThreads) reduction(+:collector)
-    {
-        int threadNum = omp_get_thread_num();
-
-        int threadRangeArr[2];
-        getThreadRange(threadNum, numThreads, arrLen, threadRangeArr);
-        #pragma omp atomic
-        collector += sumArray(threadRangeArr[1], &arr[threadRangeArr[0]]);
-    }
+    int collector = sumArrayMultithread(arrLen, arr, numThreads);
     double ss = omp_get_wtime();
 
     printf("\nN threads used: %d\nSum: %d\nElapsed time: %.8f\n\n", numThreads, collector, ss - s);
